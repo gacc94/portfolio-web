9a764d9963941da05cc4415be557ce4e5d8706fe
gacc94 <92961761+gacc94@users.noreply.github.com>
2023-07-12 09:13:37 -0500

    agregando formulario de contacto

--- src/app/public/pages/main/components/contact/contact.component.ts
+++ src/app/public/pages/main/components/contact/contact.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface IForm {
+	name: FormControl<string | null>;
+	email: FormControl<string | null>;
+	text: FormControl<string | null>;
+}
 
 @Component({
 	selector: 'gac-contact',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule],
 	templateUrl: './contact.component.html',
 	styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {}
+export class ContactComponent implements OnInit {
+	private readonly fb: FormBuilder = inject(FormBuilder);
+	form!: FormGroup<IForm>;
+
+	ngOnInit(): void {
+		this.form = this.fb.group({
+			name: ['', [Validators.required]],
+			email: ['', [Validators.required, Validators.pattern('')]],
+			text: ['', [Validators.required]]
+		});
+	}
+
+	onSubmit(): void {
+		if (!this.form.valid) {
+			this.form.markAllAsTouched();
+			return;
+		}
+		console.log(this.form.value);
+	}
+}
